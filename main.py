89347918affab01f00905d6d058034ad539900a7
Marcio Junior <marciohamer@gmail.com>
2024-07-20 16:56:29 -0300

    created coluns date with name of file

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
     from process_pandas import process_pandas
     from submit_parquet import submit_parquet
     
-    if True: #get_file()
+    if get_file():
         print('ARQUIVO BAIXADO E MOVIDO COM SUCESSO')
-        if True : #process_file() 
+        if process_file() :  
             print('ARQUIVO PROCESSADO COM SUCESSO')
             if process_pandas():  
                 print('DATAFRAME PROCESSADO COM SUCESSO')
-                if True : #submit_parquet()       
+                if submit_parquet() :       
                     print('ARQUIVO ENVIADO PARA AWS COM SUCESSO')
     
